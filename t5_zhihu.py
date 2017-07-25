fd527740faf040660bcc3dcd9a23b278dbfff54b
xuecheng.hu <hxc@aispeech.com>
2017-07-25 17:44:24 +0800

    中文字换行,英文词换行

--- t5_zhihu.py
+++ t5_zhihu.py
@@ -2,15 +2,10 @@
 #coding=utf-8
 #__author__='dahu'
 #data=2017-
-# 
-import urllib
-import urllib2
-
-url = 'https://www.zhihu.com/'
-user_agent = 'Mozilla/5.0 (X11; Linux x86_64)'
-values = {'username': '707133607@qq.com', 'password': 'XXXX'}
-headers = {'User-Agent': user_agent}
-data = urllib.urlencode(values)
-request = urllib2.Request(url, data, headers)
-response = urllib2.urlopen(request)
-page = response.read()
+#
+import re
+ss='导航上海C级W'
+for m in re.finditer('\w+',ss):
+    ss= ss.replace(m.group(),m.group()+'\n')
+    print ss
+    print [i for i in ss.decode('utf-8')]
--- t6.py
+++ t6.py
@@ -2,10 +2,49 @@
 #coding=utf-8
 #__author__='dahu'
 #data=2017-
+# 中文每行一个字,英文每行一个词,awk不大好操作
+#示例:
+#    导航去hello kitty乐园
 #
-import re
-a='I miss "\you5" '
-print a
-print re.search(r'\\',a).group()
-print re.search(r'"',a).group()
-print re.search('"',a).group()
+#      导
+#      航
+#      去
+#      hello 
+#      kitty
+#      乐
+#      园
+#      .
+#
+import re,sys
+reload(sys)
+sys.setdefaultencoding('utf8')
+def process(filename):
+    with open(filename,'r')as f:
+        for line in f:
+            yield line
+if __name__ == '__main__':
+    gen=process('tmp')
+    for line in gen:
+        if re.search('^[^"].*\w',line):
+            # print line,
+            a=[]
+            b=line.strip().decode('utf-8')  #解码,技巧,用len查看string,根据大小来判断是否要加解码
+            # print [i for i in b]
+            for i in b: #本来想直接列表推导,但是好像不支持条件判断
+                if re.search('\w',i):
+                    a.append(i)
+                elif i==' ':
+                    a.append(i)
+                else:
+                    a.append(i+'\n')
+            line=''.join(a)
+            for m in re.finditer('\w+',line):
+                # print m.span(),m.group(),line[m.start():m.end()]  #温故一下
+                line=line.replace(m.group(),m.group()+'\n')
+            line=''.join([i.strip()+'\n' for i in line.split('\n') if i!=''])    #去除空格
+            # print [i for i in line]  #列表查看下,可以看清楚每个字节到底是什么
+            print line,
+        else:
+            print line,
+
+
