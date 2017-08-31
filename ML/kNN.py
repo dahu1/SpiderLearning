58022be102404a9391c572bf55ac706dbb03eb7f
xuecheng.hu <hxc@aispeech.com>
2017-08-31 20:49:54 +0800

    change

--- ML/kNN.py
+++ ML/kNN.py
@@ -6,21 +6,35 @@
 #########################################
 # kNN: k Nearest Neighbors
 
-# Input:      inX: vector to compare to existing dataset (1xN)
-#             dataSet: size m data set of known vectors (NxM)
-#             labels: data set labels (1xM vector)
-#             k: number of neighbors to use for comparison
+# Input:      newInput: vector to compare to existing dataset (1xN)
+#             dataSet:  size m data set of known vectors (NxM)
+#             labels: 	data set labels (1xM vector)
+#             k: 		number of neighbors to use for comparison
 
 # Output:     the most popular class label
 #########################################
 
 from numpy import *
-import operator
-import os
+# import operator
+
+
+# create a dataset which contains 4 samples with 2 classes
+def createDataSet():
+    # create a matrix: each row as a sample
+    group = array([[1.0, 0.9], [1.0, 1.0], [0.1, 0.2], [0.0, 0.1]])
+    labels = ['A', 'A', 'B', 'B']  # four samples and two classes
+    return group, labels
 
 
 # classify using kNN
 def kNNClassify(newInput, dataSet, labels, k):
+    """
+    :param newInput: 
+    :param dataSet: 
+    :param labels: 
+    :param k: 
+    :return: 
+    """
     numSamples = dataSet.shape[0]  # shape[0] stands for the num of row
 
     ## step 1: calculate Euclidean distance
@@ -53,81 +67,3 @@ def kNNClassify(newInput, dataSet, labels, k):
             maxIndex = key
 
     return maxIndex
-
-
-# convert image to vector
-def img2vector(filename):
-    rows = 32
-    cols = 32
-    imgVector = zeros((1, rows * cols))
-    fileIn = open(filename)
-    for row in xrange(rows):
-        lineStr = fileIn.readline()
-        for col in xrange(cols):
-            imgVector[0, row * 32 + col] = int(lineStr[col])
-
-    return imgVector
-
-
-# load dataSet
-def loadDataSet():
-    ## step 1: Getting training set
-    print "---Getting training set..."
-    dataSetDir = '/home/dahu/PycharmProjects/SpiderLearning/ML/digits/'
-    trainingFileList = os.listdir(dataSetDir + 'trainingDigits')  # load the training set
-    numSamples = len(trainingFileList)
-
-    train_x = zeros((numSamples, 1024))
-    train_y = []
-    for i in xrange(numSamples):
-        filename = trainingFileList[i]
-
-        # get train_x
-        train_x[i, :] = img2vector(dataSetDir + 'trainingDigits/%s' % filename)
-
-        # get label from file name such as "1_18.txt"
-        label = int(filename.split('_')[0])  # return 1
-        train_y.append(label)
-
-    ## step 2: Getting testing set
-    print "---Getting testing set..."
-    testingFileList = os.listdir(dataSetDir + 'testDigits')  # load the testing set
-    numSamples = len(testingFileList)
-    test_x = zeros((numSamples, 1024))
-    test_y = []
-    for i in xrange(numSamples):
-        filename = testingFileList[i]
-
-        # get train_x
-        test_x[i, :] = img2vector(dataSetDir + 'testDigits/%s' % filename)
-
-        # get label from file name such as "1_18.txt"
-        label = int(filename.split('_')[0])  # return 1
-        test_y.append(label)
-
-    return train_x, train_y, test_x, test_y
-
-
-# test hand writing class
-def testHandWritingClass():
-    ## step 1: load data
-    print "step 1: load data..."
-    train_x, train_y, test_x, test_y = loadDataSet()
-
-    ## step 2: training...
-    print "step 2: training..."
-    pass
-
-    ## step 3: testing
-    print "step 3: testing..."
-    numTestSamples = test_x.shape[0]
-    matchCount = 0
-    for i in xrange(numTestSamples):
-        predict = kNNClassify(test_x[i], train_x, train_y, 3)
-        if predict == test_y[i]:
-            matchCount += 1
-    accuracy = float(matchCount) / numTestSamples
-
-    ## step 4: show the result
-    print "step 4: show the result..."
-    print 'The classify accuracy is: %.2f%%' % (accuracy * 100)
--- ML/t1_knn.py
+++ ML/t1_knn.py
@@ -7,7 +7,6 @@ import kNN
 from numpy import *
 
 dataSet, labels = kNN.createDataSet()
-
 testX = array([1.2, 1.0])
 k = 3
 outputLabel = kNN.kNNClassify(testX, dataSet, labels, 3)
